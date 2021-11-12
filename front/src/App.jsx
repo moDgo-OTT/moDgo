e07e9c214d20cd3f3caa75e311750b61fb7af92b
subbni <slfkak@naver.com>
2021-11-12 21:43:32 +0900

    feat: LikedClub Controller & Service 추가

---
+++ front/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Board from "../src/pages/Board";
+import Detail from "../src/pages/Detail";
+import MyPage from "./pages/MyPage";
+import ScrollToTop from "./components/common/ScrollToTop";
+import GlobalStyles from "./GlobalStyles";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <ScrollToTop>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/board" component={Board} />
+          <Route exact path="/detail/:id" component={Detail} />
+          <Route exact path="/myPage" component={MyPage} />
+        </Switch>
+      </ScrollToTop>
+    </BrowserRouter>
+  );
+};
+
+export default App;
